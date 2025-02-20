f4ed8cedfc4606fa7aabebe71bc0d2ca08be8920
diegoarnanz-maker <diegoarnanzlozano96@gmail.com>
2025-02-20 09:20:34 +0100

    Primer commit, inicializamos proyecto

---
+++ src/main/java/series_back/SeriesBackApplication.java
@@ -0,0 +1,13 @@
+package series_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeriesBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeriesBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/series_back/SeriesBackApplicationTests.java
@@ -0,0 +1,13 @@
+package series_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeriesBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
